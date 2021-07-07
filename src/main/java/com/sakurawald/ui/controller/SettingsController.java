6963fe6612adf1c722ec91bcb3ba3be6ddc2703f
HakuzosuSAMA <3172906506@qq.com>
2021-07-07 20:26:28 +0800

    UPDATE JavaFX FXMLs

--- src/main/java/com/sakurawald/ui/controller/SettingsController.java
+++ src/main/java/com/sakurawald/ui/controller/SettingsController.java
@@ -1,15 +1,519 @@
 package com.sakurawald.ui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SettingsController extends Controller {
 
+    @FXML
+    private CheckBox checkbox_real_time;
+
+    @FXML
+    private CheckBox checkbox_multi_threads;
+
+    @FXML
+    private CheckBox checkbox_raw_affects_selection;
+
+    @FXML
+    private CheckBox checkbox_show_burrowed;
+
+    @FXML
+    private CheckBox checkbox_show_cloaked;
+
+    @FXML
+    private CheckBox checkbox_trace;
+
+    @FXML
+    private CheckBox checkbox_verbose;
+
+    @FXML
+    private CheckBox checkbox_needs_support_dir;
+
+    @FXML
+    private CheckBox checkbox_raw_crop_to_playable_area;
+
+    @FXML
+    private CheckBox checkbox_use_generallized_ability_id;
+
+    @FXML
+    private TextField textfield_load_settings;
+
+    @FXML
+    private TextField textfield_window_location_left;
+
+    @FXML
+    private TextField textfield_window_location_top;
+
+    @FXML
+    private TextField textfield_data_dir;
+
+    @FXML
+    private TextField textfield_data_version;
+
+    @FXML
+    private TextField textfield_egl_path;
+
+    @FXML
+    private TextField textfield_os_mesa_path;
+
+    @FXML
+    private TextField textfieldd_process_path;
+
+    @FXML
+    private TextField textfield_temp_dir;
+
+    @FXML
+    private TextField textfield_step_size;
+
+    @FXML
+    private TextField textfield_timeout_ms;
+
+    @FXML
+    private TextField textfield_window_size_width;
+
+    @FXML
+    private TextField textfield_window_size_height;
+
+    @FXML
+    private CheckBox checkbox_disable_war_fog;
+
+    @FXML
+    private CheckBox checkbox_god_mode;
+
+    @FXML
+    private CheckBox checkbox_enemy_control;
+
+    @FXML
+    private CheckBox checkbox_give_all_resources;
+
+    @FXML
+    private CheckBox checkbox_fast_build;
+
+    @FXML
+    private CheckBox checkbox_give_all_tech;
+
+    @FXML
+    private CheckBox checkbox_give_all_upgrades;
+
+    @FXML
+    private CheckBox checkbox_ignore_food;
+
+    @FXML
+    private CheckBox checkbox_ignore_mineral;
+
+    @FXML
+    private CheckBox checkbox_ignore_resource_cost;
+
+    @FXML
+    private CheckBox checkbox_no_cooldowns;
+
+    @FXML
+    private TextField textfield_score;
+
+    @FXML
+    private Button button_score_set;
+
+    @FXML
+    private ComboBox<?> combobox_createunit_unittype;
+
+    @FXML
+    private TextField textfield_createunit_x;
+
+    @FXML
+    private TextField textfield_createunit_y;
+
+    @FXML
+    private Button button_createunit_create;
+
+    @FXML
+    private TextField textfield_createunit_var1;
+
+    @FXML
+    private TextField textfield_createunit_var2;
+
+    @FXML
+    private ComboBox<?> combobox_killunit_target_unit;
+
+    @FXML
+    private Button button_killunit_kill;
+
+    @FXML
+    private TextArea textarea_killunit_unitinfo;
+
+    @FXML
+    private ComboBox<?> combobox_draw_graphics_type;
+
+    @FXML
+    private TextField textfield_draw_point1_x;
+
+    @FXML
+    private TextField textfield_draw_point1_y;
+
+    @FXML
+    private TextField textfield_draw_point2_x;
+
+    @FXML
+    private TextField textfield_draw_point2_y;
+
+    @FXML
+    private TextField textfield_draw_value;
+
+    @FXML
+    private Button button_draw;
+
+    @FXML
+    private TextField textfield_draw_color_r;
+
+    @FXML
+    private TextField textfield_draw_color_g;
+
+    @FXML
+    private TextField textfield_draw_color_b;
+
+    @FXML
+    private TextField textfield_valuepercentagecontrol_lifepercentage;
+
+    @FXML
+    private TextField textfield_valuepercentagecontrol_shieldpercentage;
+
+    @FXML
+    private TextField textfield_valuepercentagecontrol_energypercentage;
+
+    @FXML
+    private ListView<?> list_valuespecificcontrol_unittypes;
+
+    @FXML
+    private TextField text_valuespecificcontrol_additem_life;
+
+    @FXML
+    private TextField text_valuespecificcontrol_additem_shield;
+
+    @FXML
+    private TextField text_valuespecificcontrol_additem_energy;
+
+    @FXML
+    private ComboBox<?> combobox_valuespecificcontrol_additem_unittype;
+
+    @FXML
+    private Button button_valuespecificcontrol_additem_add;
+
+    @FXML
+    void button_createunit_create_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button_draw_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button_killunit_kill_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button_score_set_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void button_valuespecificcontrol_additem_add_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_disable_war_fog_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_enemy_control_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_fast_build_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_give_all_resources_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_give_all_tech_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_give_all_upgrades_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_god_mode_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_ignore_food_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_ignore_mineral_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_ignore_resource_cost_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_multi_threads_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_needs_support_dir_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_no_cooldowns_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_raw_affects_selection_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_raw_crop_to_playable_area_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_real_time_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_show_burrowed_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_show_cloaked_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_trace_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_use_generallized_ability_id_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkbox_verbose_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void combobox_createunit_unittype_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void combobox_draw_graphics_type_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void combobox_killunit_target_unit_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void combobox_valuespecificcontrol_additem_unittype_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void text_valuespecificcontrol_additem_energy_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void text_valuespecificcontrol_additem_life_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void text_valuespecificcontrol_additem_shield_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_createunit_var1_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_createunit_var2_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_createunit_x_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_createunit_y_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_data_dir_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_data_version_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_color_b_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_color_g_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_color_r_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_point1_x_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_point1_y_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_point2_x_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_point2_y_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_draw_value_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_egl_path_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_load_settings_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_os_mesa_path_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_score_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_step_size_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_temp_dir_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_timeout_ms_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_valuepercentagecontrol_energypercentage_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_valuepercentagecontrol_lifepercentage_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_valuepercentagecontrol_shieldpercentage_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_window_location_left_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_window_location_top_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_window_size_height_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfield_window_size_width_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textfieldd_process_path_onAction(ActionEvent event) {
+
+    }
+
     public void saveAllSettings() {
-//        // Save UIConfig.
-//        AccountsConfig_Data.AccountConfig currentAccountConfig = FileManager.accountsConfig_file.getCurrentAccountConfig();
-//        currentAccountConfig.MainAccount.userID = textfield_mainaccount_user.getText();
-//        currentAccountConfig.MainAccount.password = passwordfield_mainaccount_password.getText();
-//        currentAccountConfig.SubAccount.userID = textfield_subaccount_user.getText();
-//        currentAccountConfig.SubAccount.password = passwordfield_subaccount_password.getText();
-//        currentAccountConfig.SubAccount.two_step_submit = checkbox_two_step_submit.isSelected();
-//        saveUIConfig();
     }
 }
+
